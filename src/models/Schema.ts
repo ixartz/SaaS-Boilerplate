38e59d528c64aa82b008c8a4827acb1ecff4547b
Daniel Yeoman <dyeoman2@gmail.com>
2025-06-11 14:37:42 -0700

    fix: error handling locale default and managed deprecated schema structure

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -17,31 +17,25 @@ import {
 // The migration is automatically applied during the next database interaction,
 // so there's no need to run it manually or restart the Next.js server.
 
-export const organizationSchema = pgTable(
-  'organization',
-  {
-    id: text('id').primaryKey(),
-    stripeCustomerId: text('stripe_customer_id'),
-    stripeSubscriptionId: text('stripe_subscription_id'),
-    stripeSubscriptionPriceId: text('stripe_subscription_price_id'),
-    stripeSubscriptionStatus: text('stripe_subscription_status'),
-    stripeSubscriptionCurrentPeriodEnd: bigint(
-      'stripe_subscription_current_period_end',
-      { mode: 'number' },
-    ),
-    updatedAt: timestamp('updated_at', { mode: 'date' })
-      .defaultNow()
-      .$onUpdate(() => new Date())
-      .notNull(),
-    createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
-  },
-  (table) => {
-    return {
-      stripeCustomerIdIdx: uniqueIndex('stripe_customer_id_idx').on(
-        table.stripeCustomerId,
-      ),
-    };
-  },
+export const organizationSchema = pgTable('organization', {
+  id: text('id').primaryKey(),
+  stripeCustomerId: text('stripe_customer_id'),
+  stripeSubscriptionId: text('stripe_subscription_id'),
+  stripeSubscriptionPriceId: text('stripe_subscription_price_id'),
+  stripeSubscriptionStatus: text('stripe_subscription_status'),
+  stripeSubscriptionCurrentPeriodEnd: bigint(
+    'stripe_subscription_current_period_end',
+    { mode: 'number' },
+  ),
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+});
+
+export const stripeCustomerIdIdx = uniqueIndex('stripe_customer_id_idx').on(
+  organizationSchema.stripeCustomerId,
 );
 
 export const todoSchema = pgTable('todo', {
