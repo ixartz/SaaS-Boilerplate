296ff0d1906c77461a8f119f20516ad3576b4068
Puneet Singh <puneet.singh@loginradius.com>
2025-08-10 13:21:30 +0530

    Updated sdk

--- src/lib/loginradius-react-sdk/components/ParentComponents/MagicLinkLogin.d.ts
+++ src/lib/loginradius-react-sdk/components/ParentComponents/MagicLinkLogin.d.ts
@@ -1,3 +1,5 @@
-import { MagicLinkLoginProps } from '../types';
-export declare const MagicLinkLogin: ({ onSuccess, onError, onBack, className, style, }: MagicLinkLoginProps) => import("react/jsx-runtime").JSX.Element;
+import { CaptchaProps, MagicLinkLoginProps } from '../types';
+export declare const MagicLinkLogin: ({ onSuccess, onError, onBack, className, style, captchaComponent, handleCaptcha, }: MagicLinkLoginProps & CaptchaProps) => import("react/jsx-runtime").JSX.Element;
+declare const _default: (props: MagicLinkLoginProps) => import("react/jsx-runtime").JSX.Element;
+export default _default;
 //# sourceMappingURL=MagicLinkLogin.d.ts.map
--- src/lib/loginradius-react-sdk/components/ProfileFlow.d.ts
+++ src/lib/loginradius-react-sdk/components/ProfileFlow.d.ts
@@ -1,7 +1,10 @@
+import { ApiError } from './types';
 interface ProfileProps {
-    redirectAfterLogout?: string;
+    logoutRedirectUrl?: string;
+    onSuccess?: (data: unknown) => void;
+    onError?: (error: ApiError) => void;
 }
-export declare const ProfileFlow: ({ redirectAfterLogout, }: ProfileProps) => string | import("react/jsx-runtime").JSX.Element;
+export declare const ProfileFlow: ({ logoutRedirectUrl, onSuccess, onError, }: ProfileProps) => JSX.Element;
 export declare const ProfileLoading: () => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=ProfileFlow.d.ts.map
