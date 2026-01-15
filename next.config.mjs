e9abe299451ed5ef02ffc8d3ad3879b30ea356c1
shahazad <shanudjn@gmail.com>
2026-01-15 12:28:51 +0530

    pre commit shits

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,23 @@ export default withSentryConfig(
       experimental: {
         serverComponentsExternalPackages: ['@electric-sql/pglite'],
       },
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Cross-Origin-Embedder-Policy',
+                value: 'require-corp',
+              },
+              {
+                key: 'Cross-Origin-Opener-Policy',
+                value: 'same-origin',
+              },
+            ],
+          },
+        ];
+      },
     }),
   ),
   {
