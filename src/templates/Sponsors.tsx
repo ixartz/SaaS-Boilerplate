1a0403f2c2e86c8d4b5b3b63038c01c8d4cddd67
Kaius Mak <kaicmak@mak-cloud.com>
2026-04-04 19:57:49 +0800

    style: cleanup of the unnecessary files in the template

--- src/templates/Sponsors.tsx
+++
@@ -1,8 +0,0 @@
-import { Section } from '@/features/landing/Section';
-import { SponsorLogos } from '@/features/sponsors/SponsorLogos';
-
-export const Sponsors = () => (
-  <Section>
-    <SponsorLogos />
-  </Section>
-);
