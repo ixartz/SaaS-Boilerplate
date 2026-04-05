bcc47688a41d538d006cfce5729abc9046ae9fa3
Kaius Mak <kaicmak@mak-cloud.com>
2026-04-05 15:53:47 +0800

    build: update package dependency

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/nextjs',
