e9313f79a133815b4273f4f7d8aa0351b18abdfa
Rémi Weng <ixartz@users.noreply.github.com>
2026-05-22 17:29:12 +0200

    feat: apply Cerl Shadcn theme

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,39 +1,27 @@
-'use client';
-
 import { ClerkProvider } from '@clerk/nextjs';
-import { useParams } from 'next/navigation';
-import { routing } from '@/libs/I18nRouting';
+import { shadcn } from '@clerk/ui/themes';
 import { ClerkLocalizations } from '@/utils/AppConfig';
 
-export default function AuthLayout(props: {
+export default async function AuthLayout(props: {
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = useParams<{ locale: string }>();
+  const { locale } = await props.params;
 
   const clerkLocale = ClerkLocalizations.supportedLocales[locale] ?? ClerkLocalizations.defaultLocale;
-  let signInUrl = '/sign-in';
-  let signUpUrl = '/sign-up';
-  let dashboardUrl = '/dashboard';
-  let afterSignOutUrl = '/';
-
-  if (locale !== routing.defaultLocale) {
-    signInUrl = `/${locale}${signInUrl}`;
-    signUpUrl = `/${locale}${signUpUrl}`;
-    dashboardUrl = `/${locale}${dashboardUrl}`;
-    afterSignOutUrl = `/${locale}${afterSignOutUrl}`;
-  }
 
   return (
     <ClerkProvider
       appearance={{
         cssLayerName: 'clerk', // Ensure Clerk is compatible with Tailwind CSS v4
+        theme: shadcn,
       }}
       localization={clerkLocale}
-      signInUrl={signInUrl}
-      signUpUrl={signUpUrl}
-      signInFallbackRedirectUrl={dashboardUrl}
-      signUpFallbackRedirectUrl={dashboardUrl}
-      afterSignOutUrl={afterSignOutUrl}
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+      signInFallbackRedirectUrl="/dashboard"
+      signUpFallbackRedirectUrl="/dashboard"
+      afterSignOutUrl="/"
     >
       {props.children}
     </ClerkProvider>
